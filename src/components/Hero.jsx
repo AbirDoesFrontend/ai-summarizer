615749fe96fcf8048053730921daedf330058aa3
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-13 14:06:18 +0600

    Hero Section : Navbar and Gradient Text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,38 @@
+import { logo } from "../assets";
+
+import { AiFillGithub } from "react-icons/ai";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
-export default Hero
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/AbirDoesFrontend/ai-summarizer")
+          }
+          className="black_btn flex items-center"
+        >
+          <AiFillGithub className="mr-2" size={25} />
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Tired of spending countless hours reading lengthy articles? Look no
+        further! SummarAIze is your ultimate solution for extracting the essence
+        of any text, providing you with concise and accurate summaries in a
+        matter of seconds.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
