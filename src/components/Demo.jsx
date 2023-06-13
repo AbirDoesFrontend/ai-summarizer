b81adcb05744be5fc2be46f2683b38e456a39bf4
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-14 00:37:38 +0600

    Search Url History

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,48 +2,41 @@ import { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { BsArrowReturnLeft } from "react-icons/bs";
 
-import {useLazyGetSummaryQuery} from '../services/article'
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-
   const [article, setArticle] = useState({
-    url : '',
-    summary : ''
-  })
+    url: "",
+    summary: "",
+  });
 
-  const [allArticles, setAllArticles] = useState([])
+  const [allArticles, setAllArticles] = useState([]);
 
-  const [getSummary , { error , isFetching }] = useLazyGetSummaryQuery();
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
-    )
+      localStorage.getItem("articles")
+    );
 
-    if(articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
+  }, []);
 
-  }, [])
-  
-
-  const handleSubmit = async(event) => {
-
-    event.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-
-      const updateAllArticles = [newArticle , ...allArticles]
+      const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
-      setAllArticles(updateAllArticles);
-      
-      localStorage.setItem('articles' , JSON.stringify(updateAllArticles))
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-    
-  }
+  };
 
   return (
     <section className="w-full mt-16 max-w-xl">
@@ -61,9 +54,12 @@ const Demo = () => {
             type="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange={(e) => setArticle({
-              ...article , url : e.target.value
-            })}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
             required
             className="url_input peer"
           />
@@ -75,6 +71,23 @@ const Demo = () => {
             <BsArrowReturnLeft />
           </button>
         </form>
+
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img src={copy} alt="copy-icon" className="w-[40%] h-[40%] object-contain"/>
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
